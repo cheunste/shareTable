560ef456d9dd1834fc39099e3d26f4f754a29925
Stephen <cheunste@gmail.com>
2015-03-21 15:34:24 -0700

    Added in another site Array with the names of the actual sites

--- javascript/setup.js
+++ javascript/setup.js
@@ -5,6 +5,55 @@ $(document).ready(function(){
 		'site01', 'site02', 'site03', 'site04', 'site05', 'site06', 'site07', 'site08', 'site09', 'site10', 'site11', 'site12', 'site13', 'site14', 'site15', 'site16', 'site17', 'site18', 'site19', 'site20', 'site21', 'site22', 'site23', 'site24', 'site25', 'site26', 'site27', 'site28', 'site29', 'site30', 'site31', 'site32', 'site33', 'site34', 'site35', 'site36', 'site37', 'site38', 'site39', 'site40', 'site41', 'site42', 'site43', 'site44', 'site45', 'site46', 'site47', 'site48', 'site49', 'site50'
 	];
 
+	var siteArray=[
+		'Baffin Bay',
+		'Barton',
+		'Barton Chapel',
+		'Big Horn',
+		'Big Horn II',
+		'Blue Creek',
+		'Buffalo Ridge I',
+		'Buffalo Ridge II',
+		'Casselman',
+		'Cayuga Ridge',
+		'Colorado Green',
+		'Copper Crossing',
+		'Dillon/MVIII',
+		'Dry Lake I & II',
+		'Elk River',
+		'Elm Creek',
+		'Elm Creek II',
+		'Farmers City',
+		'Flying Cloud',
+		'Groton',
+		'Hardscrabble',
+		'Hay Canyon',
+		'Hoosac',
+		'Juniper Canyon',
+		'Klondike I, II',
+		'Klondike III, IIIA',
+		'Leaning Juniper 2',
+		'Lempster',
+		'Locust Ridge',
+		'Locust Ridge II',
+		'Manzana',
+		'MinnDakota',
+		'Mopraine I & II',
+		'New Harvest',
+		'Pebble Springs',
+		'Penascal I & II',
+		'Providence Heights',
+		'Rugby',
+		'San Luis',
+		'Shiloh',
+		'South Chestnut',
+		'Star Point',
+		'Top of Iowa II',
+		'Trimont',
+		'Twin Buttes',
+		'Winnebago'
+	];
+
 	siteArray.sort();
 
 	var headerRow = $(
